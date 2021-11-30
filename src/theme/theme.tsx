4a83f55723a335b09289a7a8ada7e965beac2a14
ab-si <abhisingh7294@gmail.com>
2021-11-30 19:48:04 +0530

    Add theme

---
+++ src/theme/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main: '#fee900',
+            dark: '#fee900',
+            light:'#fee900',
+        },  
+    },
+    typography:{
+        fontFamily: "'Vcr', 'Excel'"
+    }
+});
